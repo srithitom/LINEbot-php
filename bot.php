d43a40e95920f7f5eea9d8b01f8377ff4f3155be
MagenTa <t_o_tar_dexza@windowslive.com>
2017-01-16 15:55:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,11 +16,13 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			if ($text == 'อะไร'){
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => 'what'
 			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
