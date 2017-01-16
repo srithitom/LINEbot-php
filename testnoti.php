a8e564c127fc424ebcbb87bd99bc93fe40f27682
MagenTa <t_o_tar_dexza@windowslive.com>
2017-01-16 19:14:48 +0700

    Add files via upload

---
+++ testnoti.php
@@ -0,0 +1,26 @@
+<?php 
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=สวัสดี"); 
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=hi&imageThumbnail=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png&imageFullsize=http://www.wisadev.com/wp-content/uploads/2016/08/cropped-wisadevLogo.png"); 
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer Un2LudBajEKlJs8I7qbWIMkl3f2WjGH56Yxxx9yU9zT', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
+//Close connect 
+curl_close( $chOne ); ?>
