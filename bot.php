9bef4945dff378a508c63bb17b2afbc122bc8ca9
MagenTa <t_o_tar_dexza@windowslive.com>
2017-02-17 00:04:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			$messages = [
   			      'type' => 'image',
  				'originalContentUrl' => 'https://lh3.googleusercontent.com/27aWy_6DAMilWHuWnSazb8Znu-xSG_GDpgp6MIuwvT1NkoGFFPRB0Ohd-tO4TZvahGEy9SCPts2e1oU5GOfC1uzz87q-xfJgSCNEW0A8e2iSol_8VDq4fLekb-sbV8dcKrcu8NsQKEuxHZQ4ATHgcvrEErIQMc_tt1chJ78CVP8NPPaLs0qZwjzPAA-U4T1Y2V_sHEvG6-O6xQ4k5RnI33vQLFuv_BlLNoUoYKSaAbsV2lkqOkaIjqrCUw_XefEcS0C-c9BSEYQ_GJrFnLwjI_yKCu8eCs1CeHLClRrcmYwhRLYTGk6aqPympxt6VVZqRA0yg3OfmHwMWiSoRi2BTIGccdHgvVL6rMWicj7T8vbKDQ_LCYIi7K9MB_cFyBVawKT2Q83CF4PFeDgE1EGdredtXEBhzw0NJTN9A9AJ4MYbKRBwoT5Yi-NlloMaY-WHksKICAv5OxsRd0ZTL1k8bC8vONb9XLwfYhHBmZrIIZ-rk6Z3ytoIB3IajCpOlHUR156aH0A-vTwJujFoOgUwaKuxiWbgKGCknjPdUeVN72AzhA1v-81XKVHvSpCxsbFnkVCE14Wr26jMYqw08dVKKzg01fY-fjJr4KNJGf3MYYEbZsC5Y3zK=w540-h360-no',
-  				'previewImageUrl' => 'https://1drv.ms/i/s!Akv3j4zCWiJdjmj4dwWv0clOdyXw',
+  				'previewImageUrl' => 'https://lh3.googleusercontent.com/GGkjFtyfbURVbevcchCCCzqXB82WCLJVoDriTWJhRik82r--ZCSVdhVhWQcl058WgOpVNagXVjlzdFBYaXcS8B12jAuzh69RZ2_mWHWq1UmiH9jg_cBeM33kN3GYn-o_o35l6AfqILuDiKshDxmFtYaL3G6t-ODi9Vskj5jI-MPC0oqAesNTZAS6IKOSpF74FdGtZ_7MP8u0s-9V9jngGSdvvp54WaWpq6W_AvRQUx2Z60lgitbueumI9tm2C7T4Ku_4SIVn6sQxLZ3sfyve5HvRlOHxGCS3QLN7lJ_HxU1qzilRTwcqjQg0kSXgj4CSyLlO-u-9cTkcFsy4u1cIKxr0cxMIMRueEuUtSXx8R6eMgURYOJVL7ISsd_mRa_uAX8sI0fYdZDToTK-L5Ecl24nfofQifwhtJVHDfXEU7go6HwNrtzpfsaVp75e6R1hWtEc168f6p3X6A2hEArgN6p_wzhBR3hR2CPmgWGuc1UnCaE4WA72f4cXVB1EhSrFewMZFXaIMersScRZWn0vWa8x7BmeU6Wf32ReBEEqeahlK7DWvFtVnIbibdnaKNINWv2QyNvWPUX9O6UY67W834bAMcEeRGtPA6wjmMXz-bHuebmiUr-MO=w240-h180-no',
   		 	];
 			}
 			
